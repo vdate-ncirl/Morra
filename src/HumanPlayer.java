437cd0891ca73aaf7ef01fc4d9ccb69507f56676
Cal <Cal@IBM668-PBW5D03>
2017-04-18 21:07:11 +0100

    sd

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -2,25 +2,31 @@
 import java.util.*;
 
 /**
- * This class inherits from Class Player. It implements Play method which takes input from Keyboard for a human player.
+ * This class inherits from Class Player. It implements Play method which takes
+ * input from Keyboard for a human player.
+ * 
  * @see Player
  */
 public class HumanPlayer extends Player {
-/**
- * @see Player
- * @param playerName Player Name to be set.
- */
+	/**
+	 * @see Player
+	 * @param playerName
+	 *            Player Name to be set.
+	 */
 	public HumanPlayer(String playerName) {
 		super(playerName);
 	}
-/**
- * Implements Play method which takes input from Keyboard for a human player.
- * @return Play Object .
- * @see Play
- */
+
+	/**
+	 * Implements Play method which takes input from Keyboard for a human
+	 * player.
+	 * 
+	 * @return Play Object .
+	 * @see Play
+	 */
 	@Override
 	public Play Play() {
-		Play p ;
+		Play p;
 		Scanner sc = new Scanner(System.in);
 		do {
 			/* If the user gives rubbish input, ask again --cm */
@@ -29,17 +35,17 @@ public class HumanPlayer extends Player {
 				System.out.println("Invalid entry, ONLY integers {1-10} accepted ");
 				sc.next();
 			}
-		
-		 int play = sc.nextInt();
-		 if (play <0)
-			 play *= -1;// to convert negative input to the positive value
-		 if (play >10){ // if larger than 10, then change to modulus 10
-		System.out.println("You entered a value larger than 10 - Modulated.");
-			 play = play % 10;
-		 }
-		//int playAsType = tmp;
-			//int t = s.nextInt(); 
-			//int play= tmp;
+
+			int play = sc.nextInt();
+			if (play < 0)
+				play *= -1;// to convert negative input to the positive value
+			if (play > 10) { // if larger than 10, then change to modulus 10
+				System.out.println("You entered a value larger than 10 - Modulated.");
+				play = play % 10;
+			}
+			// int playAsType = tmp;
+			// int t = s.nextInt();
+			// int play= tmp;
 			p = new Play(play);
 		} while (!ValidPlay(p));
 		AddPlayHistory(p);
