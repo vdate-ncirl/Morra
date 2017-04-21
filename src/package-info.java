61a0acdc50eede5a73329ac83b44c243aa206814
vdate <vdate@9.83.146.179>
2017-04-21 10:02:55 +0100

    updated package-info.java

--- src/package-info.java
+++ src/package-info.java
@@ -9,6 +9,33 @@ The winner of the round is decided based on the sum of fingers shown by both pla
 , namely if the sum is an even number then the “Evens” player wins, otherwise if the sum is an odd number then the “Odds” player wins.
  The winner of the round receives two points.  In addition, the player whose number of fingers is closer to the sum, receives one extra point.
 The winner of the game is the first player who accumulates six points. 
+<h3> Sample Run </h3>
+@author Vinit Date
+@author Rajeeva Revankar
+@author Carl Mohn
+ */
+/**
+ “Morra” is a hand game usually played for entertainment or to settle a disagreement. 
+ The game has many variations and can be played by two or more players. 
+
+Morra Odds and Evens Variation
+In this variation of the game, one player is going to be the “Odds” player and the other player is the “Evens” player. 
+In each round of the game, the players will simultaneously hold out between 1 and 10 fingers. 
+The winner of the round is decided based on the sum of fingers shown by both players
+, namely if the sum is an even number then the “Evens” player wins, otherwise if the sum is an odd number then the “Odds” player wins.
+ The winner of the round receives two points.  In addition, the player whose number of fingers is closer to the sum, receives one extra point.
+The winner of the game is the first player who accumulates six points. 
+
+
+<h3> Sample Run </h3>
+
+<p>Do you want to play as Even Or Odd. [Even Num entry -&gt; Even Odd Num entry -&gt; Odd]<br />5<br />Player&nbsp; : User : Choosen Playtype&nbsp; : 1<br />Player&nbsp; : PC : Choosen Playtype&nbsp; : 0<br />User please enter number between 1 and 10 <br />2<br /><br />Player&nbsp;&nbsp; &nbsp;Play&nbsp;&nbsp; &nbsp;score&nbsp;&nbsp; &nbsp;Player&nbsp;&nbsp; &nbsp;Play&nbsp;&nbsp; &nbsp;score<br />User&nbsp;&nbsp; &nbsp;2&nbsp;&nbsp; &nbsp;0&nbsp;&nbsp; &nbsp;PC&nbsp;&nbsp; &nbsp;8&nbsp;&nbsp; &nbsp;3&nbsp;&nbsp; &nbsp;<br />Total Score for User 0<br />Total Score for PC 3<br />User please enter number between 1 and 10 <br />5<br /><br />Player&nbsp;&nbsp; &nbsp;Play&nbsp;&nbsp; &nbsp;score&nbsp;&nbsp; &nbsp;Player&nbsp;&nbsp; &nbsp;Play&nbsp;&nbsp; &nbsp;score<br />User&nbsp;&nbsp; &nbsp;2&nbsp;&nbsp; &nbsp;0&nbsp;&nbsp; &nbsp;PC&nbsp;&nbsp; &nbsp;8&nbsp;&nbsp; &nbsp;3&nbsp;&nbsp; &nbsp;<br />User&nbsp;&nbsp; &nbsp;5&nbsp;&nbsp; &nbsp;1&nbsp;&nbsp; &nbsp;PC&nbsp;&nbsp; &nbsp;3&nbsp;&nbsp; &nbsp;2&nbsp;&nbsp; &nbsp;<br />Total Score for User 1<br />Total Score for PC 5<br />User please enter number between 1 and 10 <br />7<br /><br />Player&nbsp;&nbsp; &nbsp;Play&nbsp;&nbsp; &nbsp;score&nbsp;&nbsp; &nbsp;Player&nbsp;&nbsp; &nbsp;Play&nbsp;&nbsp; &nbsp;score<br />User&nbsp;&nbsp; &nbsp;2&nbsp;&nbsp; &nbsp;0&nbsp;&nbsp; &nbsp;PC&nbsp;&nbsp; &nbsp;8&nbsp;&nbsp; &nbsp;3&nbsp;&nbsp; &nbsp;<br />User&nbsp;&nbsp; &nbsp;5&nbsp;&nbsp; &nbsp;1&nbsp;&nbsp; &nbsp;PC&nbsp;&nbsp; &nbsp;3&nbsp;&nbsp; &nbsp;2&nbsp;&nbsp; &nbsp;<br />User&nbsp;&nbsp; &nbsp;7&nbsp;&nbsp; &nbsp;0&nbsp;&nbsp; &nbsp;PC&nbsp;&nbsp; &nbsp;9&nbsp;&nbsp; &nbsp;3&nbsp;&nbsp; &nbsp;<br />Total Score for User 1<br />Total Score for PC 8<br />The winner is PC</p>
+
+
+<h2> Process Flow Diagram </h2>
+<img alt="Flow Chart" src="FlowChart.jpg" ">
+
+
 @author Vinit Date
 @author Rajeeva Revankar
 @author Carl Mohn
